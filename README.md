feb6a1b3da01db5eaabff86d7ffb008db4830b1b
Eleanor Deal <eleanor.deal@gmail.com>
2022-06-20 12:25:13 +0100

    Update r script to properly get length of dataset list and document

--- README.md
+++ README.md
@@ -4,6 +4,20 @@ A place to gather helpful scripts and examples of how to extract data from ONS A
 
 ### List of datasets and their dimensions
 
+This script is intended to give a high level overview of the data available through the ONS API.
+It lists datasets, and the dimensions available within them so multiple datasets with similar 
+dimensions or characteristics can easily be identified.
+
+#### R Script
+
+Load the [dimensions-by-datasets.r](r/dimensions-by-datasets.r) script in your preferred R tooling (RStudio etc) and run. 
+
+If you do not have access to any IDE software, try copy/pasting the code [online tools](https://makemeanalyst.com/run-your-r-code/) which will run the script for you.
+
+Currently, the data is output using simple `print()` commands, so you can extend it/format the results however is most suitable.
+
+#### Executable Go Script
+
 Execute the code in the [dimensions-by-datasets](dimensions-by-datasets) directory, and pass the results to a file.
 
 > ./dimensions-by-datasets > results.txt
@@ -19,3 +33,4 @@ Life Expectancy by Local Authority
   - Label: "Time" CodeList: "two-year-intervals"
   ```
 For more details see the [README](dimensions-by-datasets/README.md).
+
--- r/dimensions-by-datasets.r
+++ r/dimensions-by-datasets.r
@@ -1,23 +1,36 @@
-#load packages
+# load packages
 library(tidyverse)
 
-#get JSON data from datasets API
-api <- jsonlite::fromJSON('https://api.beta.ons.gov.uk/v1/datasets?limit=200')
+# get JSON data from datasets API
+api <- jsonlite::fromJSON("https://api.beta.ons.gov.uk/v1/datasets?limit=200")
 
-#see what's available
-datasets <- data.frame(title = api$items$title, description = api$items$description, editions = api$items$links$editions$href, latest_version = api$items$links$latest_version$href, stringsAsFactors = FALSE)
+# see what's available
+datasets <- data.frame(
+  title = api$items$title,
+  latest_version = api$items$links$latest_version$href,
+  stringsAsFactors = FALSE
+)
 
 # loop through datasets
-for(i in 1:length(datasets))
-{
+for (i in 1:nrow(datasets)) {
   title <- datasets$title[i]
 
-  #use the latest_version link to get to the list of dimensions
-  latest_version_dimensions <- paste0(datasets$latest_version[i],'/dimensions?limit=50')
+  # use the latest_version link to get to the list of dimensions
+  latest_version_dimensions <- paste0(datasets$latest_version[i], "/dimensions?limit=50")
   dimensions_json <- jsonlite::fromJSON(latest_version_dimensions)
-  dims <- data.frame(label = dimensions_json$items$label, code_list = dimensions_json$items$links$code_list$id)
 
-  #print the title and list of dimensions for each dataset
+  # if the label (pretty-print) isn't present, use the name (URL safe) instead
+  label <- dimensions_json$items$label
+  if (length(label) == 0) {
+    label <- dimensions_json$items$name
+  }
+
+  dims <- data.frame(
+    label = label,
+    code_list = dimensions_json$items$links$code_list$id
+  )
+
+  # print the title and list of dimensions for each dataset
   print(title)
   print(dims)
 }
